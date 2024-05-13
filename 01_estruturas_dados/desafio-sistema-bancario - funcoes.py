49d5cb48383fb6d579886b296544f1c998438247
Rafael Marcondes <rafaelmarcondes@MACECOH9GWKQ05D.local>
2024-05-13 20:46:00 -0300

    feat: desafio de sistema bancario com funcoes

--- 01_estruturas_dados/desafio-sistema-bancario - funcoes.py
+++ 01_estruturas_dados/desafio-sistema-bancario - funcoes.py
@@ -1,5 +1,8 @@
 menu = """
 
+[iu] Inserir novo usuário
+[cc] Cadastrar conta
+[lc] Listar contas
 [d] Depositar
 [s] Sacar
 [e] Extrato
@@ -8,12 +11,49 @@ menu = """
 => """
 
 LIMITE_SAQUES = 3
+AGENCIA = "0001"
 limite = 500
 
 saldo = 0
 extrato = ""
 numero_saques = 0
 
+usuarios = []
+contas = []
+
+def adicionar_usuario(usuarios):
+    cpf = input("Informe o CPF (somente numeros): ")
+    if(filtrar_usuario(cpf, usuarios)):
+        print("Usuário já cadastrado.")
+        return
+
+    nome = input("Informe o nome completo: ")
+    data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+
+    print("Agora vamos ao endereço.\n")
+    rua = input("Logradouro: ")
+    numero = input("Número: ")
+    bairro = input("Bairro: ")
+    cidade = input("Cidade: ")
+    uf = input("UF: ")
+
+    endereco = f"{rua}, {numero}, {bairro}, {cidade}/{uf}"
+
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
+
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def criar_conta(agencia, numero_conta, usuarios):
+    cpf = input("Informe o CPF do usuário: ")
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if(usuario):
+        print(f"Conta {numero_conta} criada com sucesso.")
+        return {"agencia": agencia, "conta": numero_conta, "usuario": usuario}
+    
+    print(f"Usuário não encontrado. Cadastre o usuário com cpf {cpf} primeiro.")
 
 def saque(*, saldo, extrato, limite, numero_saques, limite_saques):
     excedeu_saques = numero_saques >= limite_saques
@@ -59,6 +99,21 @@ def imprimir_extrato(saldo, /, *, extrato):
     print(f"\nSaldo: R$ {saldo:.2f}")
     print("==========================================")
 
+def listar_contas(contas):
+
+    if(len(contas) == 0):
+        print("Nenhuma conta cadastrada!\n")
+        return
+
+    for conta in contas:
+        linha = f"""
+            Agência: {conta["agencia"]}
+            Conta: {conta["conta"]}
+            Titular: {conta["usuario"]["nome"]}
+        """
+        print("#" * 30)
+        print(linha)
+
 while True:
             
     opcao = input(menu)
@@ -74,6 +129,19 @@ while True:
 
     elif opcao == "q":
         break
+
+    elif opcao == "iu":
+        adicionar_usuario(usuarios)
+
+    elif opcao == "cc":
+        numero_conta = len(contas) + 1
+        conta = criar_conta(AGENCIA, numero_conta, usuarios)
+
+        if conta:
+            contas.append(conta)
+    
+    elif opcao == "lc":
+        listar_contas(contas)
 
     else:
         print("Operação inválida, por favor selecione novamente a operação desejada.")
